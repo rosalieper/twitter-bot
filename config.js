63ad05d6fd5f201adb76b2e9ded92c70982befa0
rosalie <rosalieperside@gmail.com>
2018-03-24 08:41:33 +0100

    commit to twitter-bot

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'K8oth7luvNV3OF8VhAMiV9gE3',  
+  consumer_secret: 'veACNtseSk1UKlyDHqX13CTqjO3IiErSkv1rWlryfxOPESiVw9s',
+  access_token: '782781891699175424-vqjkm5C31DwcDVBapNmhAjFwpffVQc2',  
+  access_token_secret: 'UsvpgXA4HutWIhUKbhqEUfJyJkn5AP1RM7uNjZrl8D2pH'
+}
