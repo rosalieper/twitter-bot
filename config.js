d4282d08dc1923befa34e04b5e9024a7716b885b
rosalie <rosalieperside@gmail.com>
2018-03-26 07:33:39 +0100

    fixed a few security. config file

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: 'K8oth7luvNV3OF8VhAMiV9gE3',  
-  consumer_secret: 'veACNtseSk1UKlyDHqX13CTqjO3IiErSkv1rWlryfxOPESiVw9s',
-  access_token: '782781891699175424-vqjkm5C31DwcDVBapNmhAjFwpffVQc2',  
-  access_token_secret: 'UsvpgXA4HutWIhUKbhqEUfJyJkn5AP1RM7uNjZrl8D2pH'
+  consumer_key: 'process.env.BOT_CONSUMER_KEY',  
+  consumer_secret: 'process.env.BOT_CONSUMER_SECRET',
+  access_token: 'process.env.BOT_ACCESS_TOKEN',  
+  access_token_secret: 'process.env.BOT_ACCESS_TOKEN_SECRET'
 }
